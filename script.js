b9c56d659e58b448d055a246439a63c82e078eaa
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-17 13:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
-//complete this code
-class Rectangle {}
+// Class definition for Rectangle
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    // Getter for width
+    get width() {
+        return this._width;
+    }
+
+    // Getter for height
+    get height() {
+        return this._height;
+    }
+
+    // Method to calculate and return the area of the rectangle
+    getArea() {
+        return this._width * this._height;
+    }
+}
+
+// Subclass Square that extends Rectangle
+class Square extends Rectangle {
+    constructor(side) {
+        // Call the Rectangle constructor with both width and height as the same side length
+        super(side, side);
+    }
+
+    // Method to calculate and return the perimeter of the square
+    getPerimeter() {
+        return 4 * this.width;  // Perimeter of a square is 4 times the side length
+    }
+}
+
+// Example usage:
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width);        // Output: 5
+console.log(rectangle.height);       // Output: 10
+console.log(rectangle.getArea());    // Output: 50
+
+const square = new Square(7);
+console.log(square.width);           // Output: 7
+console.log(square.height);          // Output: 7
+console.log(square.getArea());       // Output: 49
+console.log(square.getPerimeter());  // Output: 28
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
